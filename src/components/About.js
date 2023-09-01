9d64a10ad9cc130aecc933ceb37c03a570abe2f2
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-01 16:58:17 +0100

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { image } from "../data/data";
 function About() {
   return <div id="about">
     <h2>About Me</h2>
-    <p>I am Moses Oyelade, I a code everyday with Moringa, my Technical mentor is Mr. Israel Prince</p>
+    <p>I am Moses Oyelade, I code everyday with Moringa, my Technical mentor is Mr. Israel Prince</p>
     <img src={image} alt="I made this"/>
   </div>;
 }
