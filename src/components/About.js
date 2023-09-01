73a79b77c14a71924f6576743339723219c849c1
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-01 16:56:34 +0100

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am Moses Oyelade, I a code everyday with Moringa, my Technical mentor is Mr. Israel Prince</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  function handleClick(){
+    return(console.log("i am clicked"))
+  }
+
+  return (
+  <nav>
+    <a href="#home" onClick={handleClick}>Home</a>
+    <a href="#about" onClick={handleClick}>About</a>
+  </nav>
+  )
 }
 
 export default NavBar;
